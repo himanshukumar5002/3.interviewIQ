a6102ee80b1517d23b57e940e0142ed9bbafb7f3
happ <happyh43499@gmail.com>
2026-04-10 16:22:46 +0530

    updated env file

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -3,11 +3,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "interviewiq-ba6ba.firebaseapp.com",
-  projectId: "interviewiq-ba6ba",
-  storageBucket: "interviewiq-ba6ba.firebasestorage.app",
-  messagingSenderId: "862159592601",
-  appId: "1:862159592601:web:7308d702cd708076ddec08"
+  authDomain: "newproject-bac4d.firebaseapp.com",
+  projectId: "newproject-bac4d",
+  storageBucket: "newproject-bac4d.firebasestorage.app",
+  messagingSenderId: "219580569918",
+  appId: "1:219580569918:web:dd7ccdfa9dc1536479f46f",
+  measurementId: "G-FQ7RXD9L30"
 };
 
 const app = initializeApp(firebaseConfig);
